6eb19a297fcc2516ec062382ce359170f9ebc156
Francesca <francesca@pop-os.localdomain>
2023-12-07 21:16:22 +0100

    Implemented and tested movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,62 @@
+from django.core.validators import MinValueValidator, MaxValueValidator, URLValidator, RegexValidator, \
+    MinLengthValidator
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class MovieCategory(models.TextChoices):
+    ROMANCE = "ROMANCE",
+    ACTION = "ACTION",
+    ADVENTURE = "ADVENTURE",
+    COMEDY = "COMEDY"
+    CRIME = "CRIME"
+    DRAMA = "DRAMA"
+    FANTASY = "FANTASY"
+    HISTORICAL = "HISTORICAL"
+    HORROR = "HORROR"
+    MYSTERY = "MYSTERY"
+    PSYCHOLOGICAL = "PSYCHOLOGICAL"
+    SCIENCE_FICTION = "SCIENCE_FICTION"
+    THRILLER = "THRILLER"
+    WESTERN = "WESTERN"
+
+    @classmethod
+    def choices(cls):
+        print(tuple((i.name, i.value) for i in cls))
+        return tuple((i.name, i.value) for i in cls)
+
+
+class Movie(models.Model):
+
+    id = models.AutoField(
+        primary_key=True,
+        null=False,
+    )
+    title = models.CharField(
+        max_length=50,
+        validators=[MinLengthValidator(1)],
+        null=False,
+    )
+    description = models.CharField(
+        max_length=200,
+        validators=[MinLengthValidator(1)],
+        null=False,
+    )
+    year = models.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(datetime.now().year)],
+        null=False,
+    )
+    category = models.CharField(
+        max_length=256,
+        validators=[MinLengthValidator(1)],
+        null=False,
+        choices=MovieCategory.choices,
+    )
+    '''image_url = models.URLField(
+        max_length=2000,
+        validators=[URLValidator(RegexValidator(regex=r'https://%28www.%29/?[-a-zA-Z0-9@:%.+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&//=]*)'))],
+        null=False,
+    )'''
+
+    def str(self):
+        return self.title
--- tests/movies/test_models.py
+++ tests/movies/test_models.py
@@ -1,6 +1,77 @@
 import pytest
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
+from mixer.backend.django import mixer
 
-def test_mock():
-    assert True
+from movies.models import MovieCategory
+from datetime import datetime
 
 
+@pytest.mark.parametrize('title', [
+    'A' * 51,
+    ''
+])
+def test_invalid_title_raises_exception(db, title):
+    movie = mixer.blend('movies.Movie', title=title)
+    with pytest.raises(ValidationError):
+        movie.full_clean()
+
+
+def test_null_title_raises_exception(db):
+    with pytest.raises(IntegrityError):
+        mixer.blend('movies.Movie', title=None)
+
+
+@pytest.mark.parametrize('description', [
+    'A' * 201,
+    ''
+])
+def test_description_of_length_201_raises_exception(db, description):
+    movie = mixer.blend('movies.Movie', description=description)
+    with pytest.raises(ValidationError):
+        movie.full_clean()
+
+
+def test_null_description_raises_exception(db):
+    with pytest.raises(IntegrityError):
+        mixer.blend('movies.Movie', description=None)
+
+
+def test_year_prior_to_1900_raises_exception(db):
+    movie = mixer.blend('movies.Movie', year=1899)
+    with pytest.raises(ValidationError):
+        movie.full_clean()
+
+
+def test_year_after_current_year_raises_exception(db):
+    next_year = datetime.now().year + 1
+    movie = mixer.blend('movies.Movie', year=next_year)
+    with pytest.raises(ValidationError):
+        movie.full_clean()
+
+
+def test_null_year_raises_exception(db):
+    with pytest.raises(IntegrityError):
+        mixer.blend('movies.Movie', year=None)
+
+
+@pytest.mark.parametrize('category', [
+    'A' * 257,
+    '',
+    'NONEXISTENT'
+])
+def test_invalid_category_raises_exception(db, category):
+    movie = mixer.blend('movies.Movie', category='A' * 257)
+    with pytest.raises(ValidationError):
+        movie.full_clean()
+
+
+def test_null_category_raises_exception(db):
+    with pytest.raises(IntegrityError):
+        mixer.blend('movies.Movie', category=None)
+
+
+def test_valid_movie_does_not_raise_exception(db):
+    movie = mixer.blend('movies.Movie', title='A title', description='A description', year=2022,
+                        category=MovieCategory.ACTION)
+    movie.full_clean()
