9ef56e1a3e46aa0217168d9a413fb3cf0585fa98
PierKnight <45203064+PierKnight@users.noreply.github.com>
2023-12-15 13:01:27 +0100

    likes can be removed by movie_id

--- movies/permissions.py
+++ movies/permissions.py
@@ -12,3 +12,12 @@ def isReadRequest(request):
 class MoviePermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return isReadRequest(request) or isAdminUser(request)
+
+class LikePermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return True
+    
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.user_id
+    
+    
--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
-
+from rest_framework import status
+from rest_framework import permissions
+from django.shortcuts import get_object_or_404
 from movies.models import Movie, Like
-from movies.permissions import MoviePermission
+from movies.permissions import LikePermission, MoviePermission
 from movies.serializers import PublicMovieSerializer, PublicLikeSerializer
 
 
@@ -23,9 +24,17 @@ class PublicMovieViewSet(viewsets.ModelViewSet):
 
 # TODO: check and test
 class LikesViewSet(viewsets.ModelViewSet):
-    queryset = Like.objects.all()
     serializer_class = PublicLikeSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [LikePermission]
 
     def perform_create(self, serializer):
         serializer.save(user_id=self.request.user)
+    
+    def get_queryset(self):
+        return Like.objects.filter(user_id=self.request.user)
+    
+    @action(detail=False, methods=['delete'], url_path=r'by_movie/(?P<movie>[^/.]+)')
+    def remove_by_movie(self, request,movie = None):
+        instance = get_object_or_404(Like,user_id=request.user,movie=movie)
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
