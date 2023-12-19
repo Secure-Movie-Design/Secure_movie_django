82d4a76205ee6b40f06c801b0587458b0fc4f00e
Jacopo Garofalo <jacopogarofalo02@gmail.com>
2023-12-19 18:18:42 +0100

    fixed a bug with coverage on validator tests + file formatting

--- tests/movies/test_validators.py
+++ tests/movies/test_validators.py
@@ -1,7 +1,3 @@
-
-
-
-from django.forms import ValidationError
 import pytest
 from movies.validators import *
 
@@ -11,22 +7,26 @@ def test_number_validator():
     with pytest.raises(ValidationError):
         numberValidator.validate("rewrewCIaoroewewr")
 
+
 def test_uppercase_validator():
     uppercaseValidator = UppercaseValidator()
     with pytest.raises(ValidationError):
         uppercaseValidator.validate("passwordlowercase")
+
 
 def test_lowercase_validator():
     lowercaseValidator = LowercaseValidator()
     with pytest.raises(ValidationError):
         lowercaseValidator.validate("PASSWORDUPPERCASE")
 
+
 def test_symbol_validator():
     symbolValidator = SymbolValidator()
     with pytest.raises(ValidationError):
         symbolValidator.validate("Passwordwithoutsimbol43434")
 
-def test_symbol_validator():
+
+def test_max_length_validator():
     maxLenghtValidator = MaximumLengthValidator(max_length=20)
     with pytest.raises(ValidationError):
         maxLenghtValidator.validate("P" * 21)
